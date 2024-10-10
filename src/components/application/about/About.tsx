edf01107e66520fe87d7f292da4e0c5d67f7b8f6
Jessica Aguiar <jessicaag.rs@gmail.com>
2024-10-10 15:36:07 -0300

    fix: removal of home link

--- src/components/application/about/About.tsx
+++ src/components/application/about/About.tsx
@@ -5,9 +5,9 @@ export default function About() {
                 <h1 className="text-4xl mb-8">
                     Desenvolvedora <span className="font-bold text-green-500">Web</span>
                 </h1>
-                <p className="indent-8 text-justify">
+                <p className="indent-8 text-justify text-base">
                     Olá! Sou desenvolvedora web apaixonada por tecnologia, estou no mercado há mais de dois anos. Atuei
-                    em grandes empresas do ramo e em segmentos diversos, com soluções ERP, mobile e web. Nesse período
+                    em grandes empresas do ramo e em segmentos diversos, como soluções ERP, mobile e web. Nesse período
                     tive a oportunidade atuar sempre como full stack, com diversas tecnologias como Javascript,
                     Typescript, NodeJS, C#, NET, React e outras. Acredito que programação vai além do código, é entregar
                     produtos que façam a diferença para o cliente, auxiliem em suas conquistas e tragam inovação ao
--- src/components/application/navbar/SiteAreas.tsx
+++ src/components/application/navbar/SiteAreas.tsx
@@ -2,9 +2,6 @@ const SiteAreas = () => {
     return (
         <ul className="flex gap-10">
             <li className="relative after:content-[''] after:block after:w-0 after:h-1 after:bg-white after:mt-1 after:duration-500 after:ease-in-out hover:after:w-full cursor-pointer">
-                Home
-            </li>
-            <li className="relative after:content-[''] after:block after:w-0 after:h-1 after:bg-white after:mt-1 after:duration-500 after:ease-in-out hover:after:w-full cursor-pointer">
                 Sobre
             </li>
             <li className="relative after:content-[''] after:block after:w-0 after:h-1 after:bg-white after:mt-1 after:duration-500 after:ease-in-out hover:after:w-full cursor-pointer">
