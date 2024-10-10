49dd17bb4e69ee934e3c8eaa214901538d92c480
Jessica Aguiar <jessicaag.rs@gmail.com>
2024-10-10 15:36:20 -0300

    feat: add data projects

---
+++ src/data/projects.ts
@@ -0,0 +1,65 @@
+const PROJECTS = [
+    {
+        name: "Cadastro de Pacientes",
+        photo: "https://uploaddeimagens.com.br/images/004/854/699/original/Screenshot_1.png?1728583844",
+        linkSite: "https://jessicaagrs.github.io/frontend_projetoCadastroPacientes/",
+        linkCode: "https://github.com/jessicaagrs/frontend_projetoCadastroPacientes",
+        stack: [
+            "https://img.shields.io/badge/javascript-%23323330.svg?style=for-the-badge&logo=javascript&logoColor=%23F7DF1E",
+            "https://img.shields.io/badge/css3-%231572B6.svg?style=for-the-badge&logo=css3&logoColor=white",
+            "https://img.shields.io/badge/html5-%23E34F26.svg?style=for-the-badge&logo=html5&logoColor=white",
+        ],
+    },
+    {
+        name: "Site Compra de Pizzas",
+        photo: "https://uploaddeimagens.com.br/images/004/854/700/full/compra-pizzas.png?1728583871",
+        linkSite: "https://jessicaagrs.github.io/frontend_projetoCompraDePizza/",
+        linkCode: "https://github.com/jessicaagrs/frontend_projetoCompraDePizza",
+        stack: [
+            "https://img.shields.io/badge/javascript-%23323330.svg?style=for-the-badge&logo=javascript&logoColor=%23F7DF1E",
+            "https://img.shields.io/badge/css3-%231572B6.svg?style=for-the-badge&logo=css3&logoColor=white",
+            "https://img.shields.io/badge/html5-%23E34F26.svg?style=for-the-badge&logo=html5&logoColor=white",
+        ],
+    },
+    {
+        name: "Gerador de Senhas",
+        photo: "https://uploaddeimagens.com.br/images/004/854/703/full/gerador-senhas.png?1728583981",
+        linkSite: "https://jessicaagrs.github.io/frontend_projetoSistemaSenhas/",
+        linkCode: "https://github.com/jessicaagrs/frontend_projetoSistemaSenhas",
+        stack: [
+            "https://img.shields.io/badge/javascript-%23323330.svg?style=for-the-badge&logo=javascript&logoColor=%23F7DF1E",
+            "https://img.shields.io/badge/css3-%231572B6.svg?style=for-the-badge&logo=css3&logoColor=white",
+            "https://img.shields.io/badge/html5-%23E34F26.svg?style=for-the-badge&logo=html5&logoColor=white",
+        ],
+    },
+    {
+        name: "Api Controle Financeiro",
+        photo: "https://uploaddeimagens.com.br/images/004/854/719/full/api-controle-financeiro.png?1728585267",
+        linkSite: "",
+        linkCode: "https://github.com/jessicaagrs/backend_projetoControleFinanceiro",
+        stack: [
+            "https://img.shields.io/badge/c%23-%23239120.svg?style=for-the-badge&logo=csharp&logoColor=white",
+            "https://img.shields.io/badge/MongoDB-%234ea94b.svg?style=for-the-badge&logo=mongodb&logoColor=white",
+            "https://img.shields.io/badge/JWT-black?style=for-the-badge&logo=JSON%20web%20tokens",
+            "https://img.shields.io/badge/-Swagger-%23Clojure?style=for-the-badge&logo=swagger&logoColor=white",
+        ],
+    },
+    {
+        name: "Api Delivery",
+        photo: "https://uploaddeimagens.com.br/images/004/854/704/full/api-delivery.png?1728584009",
+        linkSite: "https://backend-api-delivery.vercel.app/static/index.html",
+        linkCode: "https://github.com/jessicaagrs/backend_APIDelivery",
+        stack: [
+            "https://img.shields.io/badge/fastify-%23000000.svg?style=for-the-badge&logo=fastify&logoColor=white",
+            "https://img.shields.io/badge/postgres-%23316192.svg?style=for-the-badge&logo=postgresql&logoColor=white",
+            "https://img.shields.io/badge/zod-%233068b7.svg?style=for-the-badge&logo=zod&logoColor=white",
+            "https://img.shields.io/badge/Prisma-3982CE?style=for-the-badge&logo=Prisma&logoColor=white",
+            "https://img.shields.io/badge/node.js-6DA55F?style=for-the-badge&logo=node.js&logoColor=white",
+            "https://img.shields.io/badge/typescript-%23007ACC.svg?style=for-the-badge&logo=typescript&logoColor=white",
+            "https://img.shields.io/badge/vercel-%23000000.svg?style=for-the-badge&logo=vercel&logoColor=white",
+            "https://img.shields.io/badge/-Swagger-%23Clojure?style=for-the-badge&logo=swagger&logoColor=white",
+        ],
+    },
+];
+
+export default PROJECTS;
